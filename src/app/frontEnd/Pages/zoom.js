7b72b7515245838e0bd576e2929921d341324806
HiagoP <hiagoperonip@gmail.com>
2023-03-29 17:58:47 -0300

    Instalando o Nest para começar o BackEnd

--- src/app/frontEnd/Pages/zoom.js
+++ src/app/frontEnd/Pages/zoom.js
@@ -10,23 +10,25 @@ function ZoomProd(props) {
     <main className="homePage">
       <HeaderComp />
       <h1>Freezer 1</h1>
-      <div className="dispProd">
-        <h2 className="geralProd">Status Atual</h2>
-        <div className="geralProd">
-          <h3>Temperatura Atual: -7ºC</h3>
-          <h3>Porta Fechada</h3>
+      <div className="divZoomFreezer">
+        <div className="dataZoomFreezer">
+          <h2 className="geralProd">Status Atual</h2>
+          <div className="geralProd">
+            <h3>Temperatura Atual: -7ºC</h3>
+            <h3>Porta Fechada</h3>
+          </div>
         </div>
-      </div>
-      <div>
-        <p> </p>
-      </div>
-      <div className="dispProd">
-        <h2 className="geralProd">Erros</h2>
-        <div className="geralProd">
-          <h3>Temperatura Fora do Limite</h3>
-          <h2>Horario x x</h2>
-          <h3>Porta Aberta</h3>
-          <h2>Horario x x</h2>
+        <div>
+          <p> </p>
+        </div>
+        <div className="dataZoomFreezer">
+          <h2 className="geralProd">Erros</h2>
+          <div className="geralProd">
+            <h3>Temperatura Fora do Limite</h3>
+            <h2>Horario x x</h2>
+            <h3>Porta Aberta</h3>
+            <h2>Horario x x</h2>
+          </div>
         </div>
       </div>
       <button type="button" onClick={ verProps }>Resetar</button>
